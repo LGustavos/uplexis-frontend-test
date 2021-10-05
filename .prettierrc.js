a2149c624a795e9a32ef48f22c25900958d1f118
Luiz Gustavo <luizzgustavoef@gmail.com>
2021-10-05 02:08:52 -0300

    config prettier to format code

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+    arrowParens: 'always',
+    bracketSpacing: true,
+    endOfLine: 'auto',
+    htmlWhitespaceSensitivity: 'css',
+    jsxBracketSameLine: false,
+    jsxSingleQuote: false,
+    printWidth: 80,
+    proseWrap: 'always',
+    quoteProps: 'as-needed',
+    semi: false,
+    singleQuote: true,
+    tabWidth: 2,
+    trailingComma: 'none',
+    useTabs: false,
+    vueIndentScriptAndStyle: true
+  }
