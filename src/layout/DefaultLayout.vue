7561f43fe4fda59f2ebb4d0304ea6f3056d50f2c
Luiz Gustavo <luizzgustavoef@gmail.com>
2021-10-05 02:12:53 -0300

    created default layout

---
+++ src/layout/DefaultLayout.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="main">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script></script>
+<style lang="scss">
+  * {
+    margin: 0;
+    padding: 0;
+  }
+</style>
