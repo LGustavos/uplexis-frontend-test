49d564ead6f441ed3ab750301331c06c1a07f7fe
Luiz Gustavo <luizzgustavoef@gmail.com>
2021-10-05 02:12:04 -0300

    created breadcrumb component

---
+++ src/components/Breadcrumb.vue
@@ -0,0 +1,39 @@
+<template>
+  <a class="Breadcrumb" href="/">
+    <icon icon="angle-left" class="Breadcrumb__icon"></icon>
+    <label class="Breadcrumb__label">Histórico Empresarial</label>
+  </a>
+</template>
+<script>
+  export default {}
+</script>
+<style lang="scss">
+  .Breadcrumb {
+    text-decoration: none;
+    color: $primary-color;
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+    display: flex;
+    align-items: center;
+    margin-top: 30px;
+    cursor: pointer;
+    padding-left: 120px;
+    width: 263px;
+
+    .Breadcrumb__label {
+      font-size: 25px;
+      margin-left: 8px;
+      cursor: pointer;
+    }
+
+    .Breadcrumb__icon {
+      font-size: 30px;
+    }
+  }
+
+  @media screen and (max-width: 480px) {
+    .Breadcrumb {
+      padding-left: 10px;
+    }
+  }
+</style>
